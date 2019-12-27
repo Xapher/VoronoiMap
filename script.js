21cd6bae7c86c88e1eb09c0f2a85717909e6ce9b
Xapher <schreckengostb1@gmail.com>
2019-12-26 19:46:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function load() {
   console.log("Load");
+  var canvas = document.getElementById('voronoi');
+  var ctx = canvas.getContext('2d');
+  var myImageData = ctx.createImageData(400, 400);
+  
 }
