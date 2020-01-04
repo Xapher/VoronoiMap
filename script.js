0bcd4d788b79e30c58eaa1ff95162f6baf08a4d5
Xapher <schreckengostb1@gmail.com>
2020-01-04 16:33:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ function load() {
   var ctx = canvas.getContext('2d');
   data = ctx.getImageData(0, 0, canvas.width, canvas.height);
   data[0] = 255;
+  data[1] = 255;
+  data[2] = 255;
+  data[3] = 255;
 }
