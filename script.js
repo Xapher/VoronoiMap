73188fe9c9fb875b9b375ef37c4077be970357d4
Xapher <schreckengostb1@gmail.com>
2020-01-07 17:50:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ function load() {
   var i;
   for(var x = 0; x < 100; x++) {
       for(var y = 0; y < 100; y++) {
-          console.log(distance(cx, x, cy, y)); 
+          distances = dinstances + distance(cx, x, cy, y) + "/r/n";; 
       }
   }
+  
   ctx.putImageData(data, 0, 0);
 }
-
+var distances = "<p>";
+var dend = "</p>;
 function distance(x1,x2,y1,y2) {
   return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))); 
 }
