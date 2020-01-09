d07863f1c188939becf490c5d82a52d9e63385ba
Xapher <schreckengostb1@gmail.com>
2020-01-09 16:16:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function load() {
   
   data = ctx.createImageData(100, 100);
   var i;
-  for(var x = 0; x < 100; x++) {
-      for(var y = 0; y < 100; y++) {
+  for(var x = 0; x < 10; x++) {
+      for(var y = 0; y < 10; y++) {
           distances = distances + distance(cx, x, cy, y).toString() + "/r/n"; 
       }
   }
