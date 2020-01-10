d6c754052d883aa4af85f23d47462d34c0ef02eb
Xapher <schreckengostb1@gmail.com>
2020-01-09 17:46:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var data;
 var cx = 35, cy = 82;
 var distances = "<p>";
 var dend = "</p>";
+var floats = new Array(100 * 100);
+var max = 0;
 function load() {
   
   console.log("Load");
@@ -10,12 +12,34 @@ function load() {
   
   data = ctx.createImageData(100, 100);
   var i;
+  var m = distance(cx,0,cy,0);
+  if(m > max) {
+    max = m;
+  }
+  m = distance(cx,0,cy,100);
+  if(m > max) {
+     max = m;
+  }
+  m = distance(cx,100,cy,0);
+  if(m > max) {
+    max = m;
+  }
+  m = distance(cx,100,cy,100);
+  if(m > max) {
+    max = m;
+  }
+  var count = 0;
   for(var x = 0; x < 100; x++) {
       for(var y = 0; y < 100; y++) {
-          distances = distances + distance(cx, x, cy, y).toString() + '\n'; 
+          //distances = distances + distance(cx, x, cy, y).toString() + '\n';
+          data.data[count] = (255 * (distance(cx, x, cy, y) / max));
+          data.data[count + 1] = data.data[count];
+          data.data[count + 2] = data.data[count];
+          data.data[count + 3] = 255;
+          count = count + 1;
       }
   }
-  document.getElementById('filler').innerHTML = distances + dend;
+  document.getElementById('filler').innerHTML = m;
   ctx.putImageData(data, 0, 0);
 }
 
