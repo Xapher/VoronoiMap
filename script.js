d50773c5ae1f52a44c53a6b6da9d85534738268c
Xapher <schreckengostb1@gmail.com>
2020-01-11 16:13:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,35 +5,29 @@ var distances = "<p>";
 var dend = "</p>";
 var floats = new Array(100 * 100);
 var max = 0;
+var dA, dB;
 function load() {
   
   console.log("Load");
   var canvas = document.getElementById('voronoi');
   var ctx = canvas.getContext('2d');
-  
   data = ctx.createImageData(100, 100);
   var i;
-  var m = distance(cx,0,cy,0);
-  if(m > max) {
-    max = m;
-  }
-  m = distance(cx,0,cy,100);
-  if(m > max) {
-     max = m;
-  }
-  m = distance(cx,100,cy,0);
-  if(m > max) {
-    max = m;
-  }
-  m = distance(cx,100,cy,100);
-  if(m > max) {
-    max = m;
-  }
+  var m = distance(cx,0,100,100);
   var count = 0;
+  var tD;
   for(var x = 0; x < 100; x++) {
       for(var y = 0; y < 100; y++) {
           //distances = distances + distance(cx, x, cy, y).toString() + '\n';
-          data.data[count * 4] = (255 * (1 - (distance(cx, x, cy, y) / max)));
+          dA = distance(cx, x, cy, y);
+          dB = distance(c2X, x, c2y, y);
+          if(dA < dB) {
+            tD = distance(cx, x, cy, y);
+          }
+          else {
+            tD = distance(c2x, x, c2y, y);        
+          }
+          data.data[count * 4] = (255 * (1 - (tD / max)));
           data.data[(count * 4) + 1] = data.data[count * 4];
           data.data[(count * 4) + 2] = data.data[count * 4];
           data.data[(count * 4) + 3] = 255;
