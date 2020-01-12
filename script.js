fd901f0c7f4b4772a536a6fd901b9e6c9dd2478c
Xapher <schreckengostb1@gmail.com>
2020-01-11 19:02:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 var data;
 var cx = Math.random() * 100, cy = Math.random() * 100;
 var c2x = Math.random() * 100, c2y = Math.random() * 100;
+var points = 10;
+var iX, iY;
 var distances = "<p>";
 var dend = "</p>";
-var floats = new Array(100 * 100);
 var max = 0;
 var dA = 0, dB = 0;
 function load() {
-  
+  iX = new Array(points * points);
+  iY = new Array(points * points);
   console.log("Load");
   var canvas = document.getElementById('voronoi');
   var ctx = canvas.getContext('2d');
   data = ctx.createImageData(100, 100);
   var i;
-  var m = distance(0,100,0,100);
+  var m = distance(0,100,0,100) / points;
   var count = 0;
   var tD = 100;
+  var off = 100 / points;
+  var index = 0;
+  for(var x = 0; x < points; x++) {
+    for(var y = 0; y < points; y++) {
+      var offX = ((x * off) + ((Math.random() * 10) + ((off/2))));
+      var offY = ((x * off) + ((Math.random() * 10) + ((off/2))));
+      iX[index] = offX;
+      iY[index] = offX;
+    }
+  }
+  
+  
   for(var x = 0; x < 100; x++) {
       for(var y = 0; y < 100; y++) {
           //distances = distances + distance(cx, x, cy, y).toString() + '\n';
-          dA = distance(cx, x, cy, y);
-          dB = distance(c2x, x, c2y, y);
-          if(dA < dB) {
-            tD = distance(cx, x, cy, y);
-          }
-          else {
-            tD = distance(c2x, x, c2y, y);        
+          for(var i = 0; i < points * points; i++) {
+             if(distance(offX[i], x, offY[i], y) < tD){
+               tD =  distance(offX[i], x, offY[i], y);
+             }
           }
           data.data[count * 4] = (255 * (1 - (tD / m)));
           data.data[(count * 4) + 1] = data.data[count * 4];
           data.data[(count * 4) + 2] = data.data[count * 4];
           data.data[(count * 4) + 3] = 255;
+          tD = 100;
           count = count + 1;
       }
   }
