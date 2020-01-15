02fb945d3c1a6f13cbd09399839c79d335d8b0b9
Xapher <schreckengostb1@gmail.com>
2020-01-14 18:46:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function getRandomSeeded(min,max) {
     max = max || 1;
     min = min || 0;
  
-    Math.seed = (seed * 9301 + 49297) % 233280;
+    Math.seed = (Math.seed * 9301 + 49297) % 233280;
     var rnd = Math.seed / 233280;
  
     return min + rnd * (max - min);
